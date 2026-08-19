73e3c1eac6b1fdac72e6b31eddb69b71a93f9eb5
guilherme flach <guiflach.labfsg@gmail.com>
2026-08-19 12:27:09 -0300

    adicao de historico de uso

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,3 +1,13 @@
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif 18.5 <= imc < 24.9:
+        return "Peso normal"
+    elif 25 <= imc < 29.9:
+        return "Sobrepeso"
+    else:
+        return "Obesidade"
+
 print("Calculadora de IMC")
 
 peso = float(input("Insira seu peso em kilogramas: "))
@@ -5,3 +15,9 @@ altura = float(input("Insira sua altura em metros: "))
 
 imc = peso / (altura ** 2)
 print(f"Seu IMC é: {imc:.2f}")
+
+
+
+classificacao = classificar_imc(imc)
+print(f"Classificação: {classificacao}")
+
